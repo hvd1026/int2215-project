41fb330789c60bb70ca529a6249fb933309e326e
Van Huan Dao <hvd1026@gmail.com>
2025-02-18 20:47:27 +0700

    move game loop from main.cpp to GameManager

--- src/Manager/GameManager.cpp
+++ src/Manager/GameManager.cpp
@@ -3,6 +3,7 @@
 #include <SDL_image.h>
 
 #include "GameManager.h"
+#include "TimeManager.h"
 #include "../constants.h"
 
 GameManager::GameManager()
@@ -19,57 +20,86 @@ GameManager::~GameManager()
     std::cout << "[INFO]: GameManager destroyed" << std::endl;
 }
 
-void GameManager::init(){
+void GameManager::init()
+{
     // Initialize SDL
-    if (SDL_Init(SDL_INIT_EVERYTHING) == 0){
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    {
         // Create window and renderer
         window = SDL_CreateWindow(GAME_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-        if (window){
+        if (window)
+        {
             renderer = SDL_CreateRenderer(window, -1, 0);
-            if (renderer){
+            if (renderer)
+            {
                 running = 1;
             }
         }
         // init sdl_image
-        if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
+        if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG)
+        {
             running = false;
         }
     }
 
-    if (!running) {
+    if (!running)
+    {
         std::cerr << "[ERROR]: " << SDL_GetError() << std::endl;
     }
     std::cout << "[INFO]: GameManager initialized" << std::endl;
+
+    // timer
+    timer = TimeManager::getInstance();
 }
 
-void GameManager::event(){
+void GameManager::event()
+{
     SDL_Event event;
     SDL_PollEvent(&event);
-    switch (event.type){
-        case SDL_QUIT:
-            running = 0;
-            break;
-        default:
-            break;
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        running = 0;
+        break;
+    default:
+        break;
     }
 }
 
-void GameManager::update(){
+void GameManager::update()
+{
 }
-void GameManager::render(){
+void GameManager::render()
+{
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
-    
-
 
     SDL_RenderPresent(renderer);
 }
 
-void GameManager::clean(){
+void GameManager::clean()
+{
     SDL_DestroyWindow(window);
     window = NULL;
     SDL_DestroyRenderer(renderer);
     renderer = NULL;
+    timer->clean();
+    timer = NULL;
     SDL_Quit();
     IMG_Quit();
+}
+
+void GameManager::run()
+{
+    while (running)
+    {
+        timer->update();
+        event();
+        update();
+        if (timer->getDeltaTime() >= (1.0f / FPS))
+        {
+            render();
+            timer->reset();
+        }
+    }
 }
